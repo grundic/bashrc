0259c9b2778a39fd18f71e2968121e5287b88b84
Grigory Chernyshev <systray@yandex.ru>
2013-04-28 13:16:09 +0400

    some useful stuff

---
+++ bash_aliases.d/comfortable.bash
@@ -0,0 +1,8 @@
+# Various handy shortcuts and helpers
+
+alias h='history'
+alias j='jobs -l'
+# show each item of $PATH on new line
+alias path='echo -e ${PATH//:/\\n}'
+# show open ports
+alias ports='netstat -tulanp'
