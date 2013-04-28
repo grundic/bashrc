c886dede9facb1ccee00ae4b147fc7daa41e868f
Grigory Chernyshev <systray@yandex.ru>
2013-04-28 13:38:13 +0400

    git aliases

---
+++ bash_aliases.d/git.bash
@@ -0,0 +1,9 @@
+alias g='git'
+alias gcl='git clone'
+alias gs='git status'
+alias gll='git log --graph --pretty=oneline --abbrev-commit'
+alias gg="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
+alias ggs="gg --stat"
+alias gsl="git shortlog -sn"
+alias gw="git whatchanged"
+
